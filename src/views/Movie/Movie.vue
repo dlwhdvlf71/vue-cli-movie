8d94129067715285f7c88602d83b9182ee03e7bc
JungJuYoung <insert@kakao.com>
2020-09-19 22:42:41 +0900

    화살표 함수와 일반 함수의 차이

--- src/views/Movie/Movie.vue
+++ src/views/Movie/Movie.vue
@@ -5,40 +5,49 @@
             {{movie.title}}
         </li>
     </ul>
-   
 </template>
 
 
 <script>
-
+const axios = require('axios')
 const movieKey = '2d8280011e977622269e13664d038055';
 
-const axios = require('axios');
-
 export default {
-        name : 'movies',
-        data () {
-            return {
-                nowplaying : []
-            }
-        },
-        mounted () {
-            axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${movieKey}&language=en-US&page=1`)
-            .then(function (response) {
-                // handle success
-                console.log(response.data.results);
-
-                this.nowplaying = response.data.results;
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            })
-            .then(function () {
-                // always executed
-            });
+    name : 'movies',
+    data : function () {
+        return {
+            nowplaying : []
         }
-    
+    },
+    mounted : function () {
+        /**
+         * 1. regular function 를 이용한 방법
+         * regular function 를 쓰는 경우 scope 가 변경되어 this.nowPlaying 에 접근이 불가하다.
+         * 바깥쪽에 'const 변수명 = this' 선언하여 전역으로 this 를 사용하게 하자
+         */
+        // const self = this
+        // axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${movieKey}&language=en-US&page=1`)
+        // .then(function (response) {
+        //     self.nowplaying = response.data.results
+        // })
+        // .catch(function(error) {
+        //     // handle error
+        //     console.log(error);
+        // })
+
+        /**
+         * 2. arrow function 를 이용한 방법
+         * 참고 URL : https://ko.javascript.info/arrow-functions
+         */
+        axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${movieKey}&language=en-US&page=1`)
+        .then((response) => {
+            this.nowplaying = response.data.results
+        })
+        .catch((error) => {
+            // handle error
+            console.log(error);
+        })
+    }
 }
 
 </script>
