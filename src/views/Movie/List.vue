0010fea4cdadb4a1350b544c56197e658ecaec11
dlwhdvlf71 <dlwhdvlf71@gmail.com>
2020-09-20 16:40:57 +0900

    List vue

---
+++ src/views/Movie/List.vue
@@ -0,0 +1,46 @@
+<template>
+    <ul>
+        <li v-for="movie in nowPlaying" :key="movie.id">
+            {{movie.title}}
+            <posterComponents :poster-path="movie.poster_path"> </posterComponents>
+        </li>
+    </ul>
+</template>
+
+<script>
+
+import posterComponents from './Poster.vue'
+
+const axios = require('axios');
+const apiKey = '2d8280011e977622269e13664d038055';
+
+export default {
+
+    data : function () {
+        return {
+            nowPlaying : []
+        }
+    },
+    components : {
+        posterComponents
+    },
+    mounted : function () {
+        axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=`)
+        .then((response) => {
+            // handle success
+            //console.log(response.data.results);
+            console.log("now playing")
+
+            this.nowPlaying = response.data.results;
+        })
+        .catch((error) => {
+            // handle error
+            console.log(error);
+        })
+        .then(() => {
+            // always executed
+        })
+    }
+
+}
+</script>>
--- src/views/Movie/Movie.vue
+++ src/views/Movie/Movie.vue
@@ -1,48 +1,21 @@
 
 <template>
-    <ul>
-        <li v-for="movie in nowPlaying" :key="movie.id">
-            {{movie.title}}
-            <!-- <img width="300px" height="300px" :src="'https://image.tmdb.org/t/p/original' + movie.poster_path" > -->
-            <posterComponents :poster-path="movie.poster_path"> </posterComponents>
-        </li>
-    </ul>
+    
+    <ListComponents> </ListComponents>
+
 </template>
 
 <script>
 
-import posterComponents from './Poster.vue'
+import ListComponents from './List.vue'
 
-const axios = require('axios');
-const apiKey = '2d8280011e977622269e13664d038055';
 
 export default {
 
-    data : function () {
-        return {
-            nowPlaying : []
-        }
-    },
+    name : 'Movie',
     components : {
-        posterComponents
-    },
-    mounted : function () {
-        axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=`)
-        .then((response) => {
-            // handle success
-            //console.log(response.data.results);
-            console.log("now playing")
-
-            this.nowPlaying = response.data.results;
-        })
-        .catch((error) => {
-            // handle error
-            console.log(error);
-        })
-        .then(() => {
-            // always executed
-        })
+        ListComponents
     }
-
+    
 }
 </script>>
