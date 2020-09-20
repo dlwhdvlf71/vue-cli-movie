c7e0ff125f04a99c6c0e0f7403395845b72610ea
dlwhdvlf71 <dlwhdvlf71@gmail.com>
2020-09-20 15:23:42 +0900

    print nowPlaying List

--- src/views/Movie/Movie.vue
+++ src/views/Movie/Movie.vue
@@ -1,13 +1,43 @@
 
 <template>
-    <div>
-        Movie
-    </div>
+    <ul>
+        <li v-for="movie in nowPlaying" :key="movie.id">
+            {{movie.title}}
+        </li>
+    </ul>
 </template>
 
 
 <script>
+
+const axios = require('axios');
+const apiKey = '2d8280011e977622269e13664d038055';
+
 export default {
 
+    data : function () {
+        return {
+            nowPlaying : []
+        }
+    },
+    mounted : function () {
+        axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=`)
+        .then((response) => {
+            // handle success
+            //console.log(response.data.results);
+            console.log("now playing")
+
+            this.nowPlaying = response.data.results;
+        })
+        .catch((error) => {
+            // handle error
+            console.log(error);
+        })
+        .then(() => {
+            // always executed
+        })
+
+    }
+
 }
-</script>
+</script>>
