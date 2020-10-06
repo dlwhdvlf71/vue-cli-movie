8c90910abf7aafda38a2cd788147b092d264c595
JongPil Lee <dlwhdvlf71@gmail.com>
2020-10-06 18:03:43 +0900

    add TV

---
+++ src/components/TV/AiringToday.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="wrapper-Popular">
+        <h1>
+            <div>오늘 방송 프로그램</div>
+        </h1>
+        <div>
+            <SwiperComponetns :propsDatas="datas"></SwiperComponetns>
+        </div>
+    </div>
+</template>
+
+
+<script>
+
+import SwiperComponetns from './Controls/Swiper.vue';
+
+const axios = require('axios');
+const apiKey = '2d8280011e977622269e13664d038055';
+
+export default {
+    name : 'TVAiringToday',
+    data() {
+        return {
+            datas : []
+        }
+    },
+    components : {
+        SwiperComponetns
+    },
+    mounted() {
+      axios.get(`https://api.themoviedb.org/3/tv/airing_today?api_key=${apiKey}&language=en-US&page=1`)
+        .then((response) => {
+            // handle success
+
+            this.datas = response.data.results;
+
+        })
+        .catch((error) => {
+            // handle error
+            console.log(error);
+        })
+        .then(() => {
+            // always executed
+        })
+    }
+}
+</script>
+
+
+<style>
+    .wrapper-Popular{
+
+
+    }
+</style>
--- src/components/TV/Main.vue
+++ src/components/TV/Main.vue
@@ -1,11 +1,15 @@
 <template>
     <div class="wrappe-_main">
         <ThumnailComponents></ThumnailComponents>
-
         <div>
             <PopularComponents></PopularComponents>
         </div>
-        
+        <div>
+            <AiringTodayComponents></AiringTodayComponents>
+        </div>
+        <div>
+            <OnTheAirComponents></OnTheAirComponents>
+        </div>
     </div>
 </template>
 
@@ -14,11 +18,15 @@
 
 import ThumnailComponents from './Thumnail.vue';
 import PopularComponents from './Popular.vue';
+import AiringTodayComponents from './AiringToday.vue';
+import OnTheAirComponents from './OnTheAir.vue';
 
 export default {
     components : {
         ThumnailComponents,
-        PopularComponents
+        PopularComponents,
+        AiringTodayComponents,
+        OnTheAirComponents
     }
 }
 </script>
